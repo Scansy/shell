c8c86e3bdad2a79651a87a4d00d5116d788f5a55
Matthew Widjaja <matthewphilip123@gmail.com>
2025-05-19 13:50:01 -0700

    Background jobs in process..

--- main.go
+++ main.go
@@ -7,10 +7,26 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"regexp"
 )
 
+// BackgroundProcess is a struct that contains a meta data of a background process.
+type BackgroundProcess struct {
+	Cmd *exec.Cmd		// Cmd datatype
+	Command string		// The command being run
+	Input string		// The inputted line to run the command
+}
+
 func main() {
+	// Variables
+	var processes map[string]BackgroundProcess = make(map[string]BackgroundProcess) // input : BackgroundProcess
+	var isBackground bool = false
+	doneJobs := make(chan string)
 	reader := bufio.NewReader(os.Stdin)
+	
+	// Regex
+	backgroundRegex := regexp.MustCompile(`\[(.*?)\]`)
+
 
 	for {
 		prompt, err := getPromptInfo()
@@ -24,19 +40,54 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		// Joins any goroutines
+		select {
+			case msg := <-doneJobs:
+				fmt.Println("Background job finished: ", msg)
+				match := backgroundRegex.FindStringSubmatch(msg)
+				if len(match) > 1 { // A sub-match is found
+					delete(processes, match[1])
+				}
+
+			default: // nothing to join
+
+		}
+		fmt.Printf("Num of bg processes: %d\n", len(processes)) // check if bg processes was handled properly
+		fmt.Println(processes)
 		
+		// Skips empty inputs
 		if len(input) == 0 {
 			continue
 		}
 
 		// PARSE
-		var args []string = strings.Fields(input)// returns a slice (no size in square brackets)
+		var args []string = strings.Fields(input) // returns a slice (no size in square brackets)
+		fmt.Printf("last arg: %v\n", args[len(args)-1])
+		if args[len(args)-1] == "&" {
+			isBackground = true
+			args = args[:len(args) - 1] // remove ampersand
+		}
 
 		// EXECUTE
 		var cmd *exec.Cmd = exec.Command(args[0], args[1:]...) // ellipsis syntax to "spread out" the slice
 		cmd.Stdout = os.Stdout
 		cmd.Stdin = os.Stdin
-		cmd.Run()
+
+		if !isBackground {
+			err := cmd.Run()
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			var backgroundProcess BackgroundProcess
+			backgroundProcess.Cmd = cmd
+			backgroundProcess.Command = args[0]
+			backgroundProcess.Input = input
+			processes[input] = backgroundProcess
+			runBackgroundProcess(backgroundProcess, doneJobs)
+			isBackground = false
+		}
 	}
 
 }
@@ -46,6 +97,7 @@ func getPromptInfo() (string, error) {
 	var prompt string
 	hostname, err := os.Hostname()
 	if err != nil {
+		return prompt, err
 	}
 
 	pwd, err := os.Getwd()
@@ -65,4 +117,23 @@ func getPromptInfo() (string, error) {
 	}
 	prompt = hostname + " || " + pwd + "$ "
 	return prompt, nil
+}
+
+// runBackgroundProcess launches a goroutine to start and wait for a process in the background
+func runBackgroundProcess(bp BackgroundProcess, done chan<- string) {
+	go func() {
+		err := bp.Cmd.Start()
+		if err != nil {
+			log.Fatal(err)
+		} 
+		
+		err = bp.Cmd.Wait()
+		fmt.Print("Done waiting..")
+		if err != nil {
+            done <- fmt.Sprintf("Job [%s] exited with error: %v", bp.Input, err)
+        } else {
+            done <- fmt.Sprintf("Job [%s] completed successfully", bp.Input)
+        }
+
+	}()
 }
